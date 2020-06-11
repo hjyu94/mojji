d212a01be27edfd9f6e5d4dccfa6fd7ceb645187
효정 <hjeong.you@gmail.com>
2020-06-11 15:10:33 +0900

    프로젝트 세팅

---
+++ src/main/java/me/hjeong/mojji/MojjiApplication.java
@@ -0,0 +1,13 @@
+package me.hjeong.mojji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MojjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hjeong/mojji/MojjiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjeong.mojji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
