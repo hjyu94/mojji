dc4b6e88463db8c01e9fcbd2ab840e21429ea5cf
효정 <hjeong.you@gmail.com>
2020-07-07 12:58:50 +0900

    QueryDSL 적용

--- src/main/java/me/hjeong/mojji/post/PostRepository.java
+++ src/main/java/me/hjeong/mojji/post/PostRepository.java
@@ -1,14 +1,13 @@
 package me.hjeong.mojji.post;
 
 import me.hjeong.mojji.domain.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Transactional(readOnly = true)
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
     List<Post> findFirst6ByOrderByCreatedDateTimeDesc();
 }
