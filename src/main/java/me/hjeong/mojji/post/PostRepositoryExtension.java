1bf166ecb6dc416153c2cbad1dd40b524b258241
효정 <hjeong.you@gmail.com>
2020-07-07 18:27:38 +0900

    account, isSold 로 Post 가져오는 쿼리 추가

--- src/main/java/me/hjeong/mojji/post/PostRepositoryExtension.java
+++ src/main/java/me/hjeong/mojji/post/PostRepositoryExtension.java
@@ -1,5 +1,6 @@
 package me.hjeong.mojji.post;
 
+import me.hjeong.mojji.domain.Account;
 import me.hjeong.mojji.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface PostRepositoryExtension {
     Page<Post> findByKeywords(Pageable pageable, List<String> keywords);
+    List<Post> findPostsBySellerAndIsSold(Account account, boolean isSold);
 }
--- src/main/java/me/hjeong/mojji/post/PostRepositoryExtensionImpl.java
+++ src/main/java/me/hjeong/mojji/post/PostRepositoryExtensionImpl.java
@@ -3,10 +3,8 @@ package me.hjeong.mojji.post;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.JPQLQuery;
-import me.hjeong.mojji.domain.Post;
-import me.hjeong.mojji.domain.QCategory;
-import me.hjeong.mojji.domain.QPost;
-import me.hjeong.mojji.domain.QStation;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.hjeong.mojji.domain.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -14,16 +12,19 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 import java.util.List;
 
+import static me.hjeong.mojji.domain.QPost.post;
+
 public class PostRepositoryExtensionImpl extends QuerydslRepositorySupport implements PostRepositoryExtension {
 
-    public PostRepositoryExtensionImpl() {
+    private final JPAQueryFactory queryFactory;
+
+    public PostRepositoryExtensionImpl(JPAQueryFactory queryFactory) {
         super(Post.class);
+        this.queryFactory = queryFactory;
     }
 
     @Override
     public Page<Post> findByKeywords(Pageable pageable, List<String> keywords) {
-        QPost post = QPost.post;
-
         Predicate[] predicates = new Predicate[keywords.size()];
         for(int i=0; i<keywords.size(); ++i) {
             String keyword = keywords.get(i);
@@ -43,5 +44,12 @@ public class PostRepositoryExtensionImpl extends QuerydslRepositorySupport imple
         JPQLQuery<Post> pageableQuery = getQuerydsl().applyPagination(pageable, postJPQLQuery);
         QueryResults<Post> fetchResults = pageableQuery.fetchResults();
         return new PageImpl<>(fetchResults.getResults(), pageable, fetchResults.getTotal());
+    }
+
+    @Override
+    public List<Post> findPostsBySellerAndIsSold(Account account, boolean isSold) {
+        return queryFactory.selectFrom(post)
+                .where(post.seller.eq(account).and(post.isSold.eq(isSold)))
+                .fetch();
     }
 }
