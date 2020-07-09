712d538228d5d6f9082afe7ae6388a72ce74d5b1
효정 <hjeong.you@gmail.com>
2020-07-09 21:32:59 +0900

    스프링 시큐리티 로그인 테스트, notiCount 가 뷰에 표시되는지 테스트

--- src/test/java/me/hjeong/mojji/factory/NotificationFactory.java
+++ src/test/java/me/hjeong/mojji/factory/NotificationFactory.java
@@ -1,7 +1,10 @@
 package me.hjeong.mojji.factory;
 
+import me.hjeong.mojji.domain.Account;
+import me.hjeong.mojji.domain.Category;
 import me.hjeong.mojji.domain.Notification;
 import me.hjeong.mojji.notification.NotificationRepository;
+import me.hjeong.mojji.notification.NotificationType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +13,20 @@ import java.util.List;
 @Component
 public class NotificationFactory {
 
-    @Autowired
-    NotificationRepository notificationRepository;
+    @Autowired NotificationRepository notificationRepository;
 
     public Integer getTotalSize() {
         List<Notification> all = notificationRepository.findAll();
         return all.size();
+    }
+
+    public Notification createNotification(Account account, NotificationType type) {
+        Notification notification = Notification.builder()
+                .title("알림 메세지 제목임당")
+                .account(account)
+                .type(type)
+                .build();
+        return notificationRepository.save(notification);
     }
 
 }
