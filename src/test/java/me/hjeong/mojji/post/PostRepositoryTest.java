3f0de2103b7acf38734073c10a26d3818b2cf6c7
효정 <hjeong.you@gmail.com>
2020-07-09 17:42:27 +0900

    [테스트] PostRepository 최신 게시물 6개 가져오기

--- src/test/java/me/hjeong/mojji/post/PostRepositoryTest.java
+++ src/test/java/me/hjeong/mojji/post/PostRepositoryTest.java
@@ -1,22 +1,32 @@
 package me.hjeong.mojji.post;
 
+import me.hjeong.mojji.config.DataJpaConfig;
 import me.hjeong.mojji.domain.Post;
+import me.hjeong.mojji.factory.PostFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
 @DataJpaTest
+@Import(DataJpaConfig.class)
 class PostRepositoryTest {
 
     @Autowired PostRepository postRepository;
+    @Autowired PostFactory postFactory;
 
     @Test
     void findAllOrderByCreatedDateTimeDesc() {
+        for(int i=0; i<30; ++i) {
+            postFactory.createPost("게시물-" + i);
+        }
         List<Post> posts = postRepository.findFirst6ByOrderByCreatedDateTimeDesc();
-        System.out.println("******************" + Arrays.toString(posts.toArray()));
+        assertThat(posts, hasSize(6));
     }
 
 }
