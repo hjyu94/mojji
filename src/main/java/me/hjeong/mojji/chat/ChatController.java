356082f295fc8adbbe6aa8c18a653595bede7d75
효정 <hjeong.you@gmail.com>
2020-07-14 14:03:24 +0900

    메세지 뷰

--- src/main/java/me/hjeong/mojji/chat/ChatController.java
+++ src/main/java/me/hjeong/mojji/chat/ChatController.java
@@ -1,7 +1,10 @@
 package me.hjeong.mojji.chat;
 
 import lombok.RequiredArgsConstructor;
+import me.hjeong.mojji.account.CurrentAccount;
+import me.hjeong.mojji.domain.Account;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,8 +16,9 @@ public class ChatController {
     private final ChatService service;
 
     @GetMapping("/chats")
-    public void getChatRooms() {
-
+    public String getChatRooms(@CurrentAccount Account account, Model model) {
+        model.addAttribute("account", account);
+        return "chats";
     }
 
     @GetMapping("/chat/{account-id}")
